580e328648c849947c9cf85b66a42c52c4f12d0b
Tareq Hassan <tareqhassan2014@gmail.com>
2021-03-16 14:23:03 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useState } from 'react';
+import firebaseConfig from './firebase.config';
+
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 function App() {
+  const [user, setUser] = useState({})
+
+
+  // handel Google SignIn
+  const handelGoogleSignIn = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        var user = result.user;
+        setUser(user);
+      }).catch((error) => {
+        var errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  }
+
+  // handel Facebook SignIn
+  const handelFacebookSignIn = () => {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        var user = result.user;
+        setUser(user)
+      })
+      .catch((error) => {
+        var errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  }
+
+
+  // handelGitHUbSignIn
+  const handelGitHUbSignIn = () => {
+    const provider = new firebase.auth.GithubAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        var user = result.user;
+        setUser(user)
+      })
+      .catch((error) => {
+        var errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handelGoogleSignIn}>Sign In With Google</button>
+      <br />
+      <button onClick={handelGitHUbSignIn}>Sign In With GitHUb</button>
+      <br />
+      <button onClick={handelFacebookSignIn}>Sign In With Facebook</button>
+      <p>User: {user.displayName}</p>
+      <p>Email: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAb9FVgu0rnUM-C3aAHggFMv8-16B4fu0w",
+  authDomain: "auth-recap-ed4ae.firebaseapp.com",
+  projectId: "auth-recap-ed4ae",
+  storageBucket: "auth-recap-ed4ae.appspot.com",
+  messagingSenderId: "772669831615",
+  appId: "1:772669831615:web:77f7675e95efbfaee4490a"
+};
+
+export default firebaseConfig;
